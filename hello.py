abc6224c0dff6db487cc77dfcd53ea1f4aed0475
lixiang30 <534551900@qq.com>
2019-01-07 23:19:59 +0800

    日志和错误管理

--- hello.py
+++ hello.py
@@ -14,11 +14,21 @@ def hello_world():
 
     url_2 = UrlManager.buildStaticUrl("/css/bootstrap")
 
+    msg = "hello word,url:%s;url_1:%s;url_2:%s" % (url,url_1,url_2)
+    app.logger.info(msg)
+    app.logger.error(msg)
+
     return "hello word,url:%s;url_1:%s;url_2:%s" % (url,url_1,url_2)
 
 @app.route("/api")
 def index():
     return "index page"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.error(error)
+    return "This page does not exist",404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
