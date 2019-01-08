5924d2a9e6c89010e7b5a2e61a968b3db92b42c3
lixiang30 <534551900@qq.com>
2019-01-08 21:10:35 +0800

    后台页面的搭建

--- common/libs/UrlManager.py
+++ common/libs/UrlManager.py
@@ -1,10 +1,27 @@
 
+# class UrlManager(object):
+#     @staticmethod
+#     def buildUrl(path):
+#         return path
+#
+#     @staticmethod
+#     def buildStaticUrl(path):
+#         path = path + "?ver=" + "xx年xx月xx日"
+#         return path
+
+
+# -*- coding: utf-8 -*-
+
 class UrlManager(object):
+    def __init__(self):
+        pass
+
     @staticmethod
-    def buildUrl(path):
+    def buildUrl( path ):
         return path
 
     @staticmethod
     def buildStaticUrl(path):
-        path = path + "?ver=" + "xx年xx月xx日"
-        return path
+        ver = "%s"%( 22222222 )
+        path =  "/static" + path + "?ver=" + ver
+        return UrlManager.buildUrl( path )
